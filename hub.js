cc0e5fc5ed47acd6657d5da3e21d44f01a31c956
Jens N <jens.n@godlike.biz>
2022-07-31 21:36:59 +0200

    Added wiki

--- hub.js
+++ hub.js
@@ -1,45 +1,30 @@
 (function($) {
 
-    // Logistics calculator
-    $("#logistics-calc-btn").each(function() {
-        let tabFrame = $("#logistics-calc-tab iframe");
-        const tabBootstrap = new bootstrap.Tab(this);
-        this.addEventListener('click', event => {
-            event.preventDefault();
-            tabBootstrap.show();
-            if (!tabFrame.is(".loaded")) {
-                tabFrame.addClass("loaded");
-                tabFrame.attr("src", "https://www.ogma.run/factory");
-            }
+    let bindTab = function(ident, url) {
+        $("#"+ident+"-btn").each(function() {
+            let tabFrame = $("#"+ident+"-tab iframe");
+            const tabBootstrap = new bootstrap.Tab(this);
+            this.addEventListener('click', event => {
+                event.preventDefault();
+                tabBootstrap.show();
+                if (!tabFrame.is(".loaded")) {
+                    tabFrame.addClass("loaded");
+                    tabFrame.attr("src", url);
+                }
+            });
         });
-    });
+    };
+
+    // Logistics calculator
+    bindTab("logistics-calc", "https://www.ogma.run/factory");
 
     // Route planner
-    $("#route-planner-btn").each(function() {
-        let tabFrame = $("#route-planner-tab iframe");
-        const tabBootstrap = new bootstrap.Tab(this);
-        this.addEventListener('click', event => {
-            event.preventDefault();
-            tabBootstrap.show();
-            if (!tabFrame.is(".loaded")) {
-                tabFrame.addClass("loaded");
-                tabFrame.attr("src", "https://www.logiwaze.com/");
-            }
-        });
-    });
+    bindTab("route-planner", "https://www.logiwaze.com/");
+
+    // Foxhole wiki
+    bindTab("wiki", "https://foxhole.fandom.com/wiki/Foxhole_Wiki");
 
     // Artillery spotting
-    $("#artillery-spotting-btn").each(function() {
-        let tabFrame = $("#artillery-spotting-tab iframe");
-        const tabBootstrap = new bootstrap.Tab(this);
-        this.addEventListener('click', event => {
-            event.preventDefault();
-            tabBootstrap.show();
-            if (!tabFrame.is(".loaded")) {
-                tabFrame.addClass("loaded");
-                tabFrame.attr("src", "https://forsakenngs.github.io/foxhole-spotter/");
-            }
-        });
-    });
+    bindTab("artillery-spotting", "https://forsakenngs.github.io/foxhole-spotter/");
 
 })(jQuery);
